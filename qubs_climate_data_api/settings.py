052b6a17ad5ce8a188b83322bc3a991c05c4489c
David Lougheed <david.lougheed@gmail.com>
2016-08-15 12:11:49 -0400

    Change database information in global settings file.

--- qubs_climate_data_api/settings.py
+++ qubs_climate_data_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'qubs_climate_data_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'qubs_climate_manager',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
