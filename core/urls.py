465fd8892c177925d8da3080d08676daad866195
David Lougheed <david.lougheed@gmail.com>
2016-08-15 12:04:09 -0400

    Add URLs for previous views.

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,21 @@ from django.conf.urls import url
 from core import views
 
 urlpatterns = [
-	url(r'^sensors/$', views.SensorList.as_view()),
-	url(r'^sensors/(?P<pk>[0-9]+)/$', views.SensorDetail.as_view()),
+    url(r'^$', views.api_root),
 
-	url(r'^stations/$', views.StationList.as_view()),
-	url(r'^stations/(?P<pk>[0-9]+)/$', views.StationDetail.as_view()),
+    url(r'^sensors/$', views.SensorList.as_view(), name='sensor-list'),
+    url(r'^sensors/(?P<pk>[0-9]+)/$', views.SensorDetail.as_view(), name='sensor-detail'),
+    url(r'^sensors/(?P<pk>[0-9]+)/data/$', views.SensorData.as_view(), name='sensor-data'),
 
-	url(r'^readings/$', views.ReadingList.as_view()),
-	url(r'^readings/(?P<pk>[0-9]+)/$', views.ReadingDetail.as_view()),
+    url(r'^stations/$', views.StationList.as_view(), name='station-list'),
+    url(r'^stations/(?P<pk>[0-9]+)/$', views.StationDetail.as_view(), name='station-detail'),
+
+    url(r'^readings/$', views.ReadingList.as_view(), name='reading-list'),
+    url(r'^readings/(?P<pk>[0-9]+)/$', views.ReadingDetail.as_view(), name='reading-detail'),
+
+    url(r'^messages/$', views.MessageList.as_view(), name='message-list'),
+    url(r'^messages/(?P<pk>[0-9]+)/$', views.MessageDetail.as_view(), name='message-detail'),
+
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
 ]
