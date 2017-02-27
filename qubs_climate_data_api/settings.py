048f2f49ce37e8be700f7337f088a41a98527211
David Lougheed <david.lougheed@gmail.com>
2017-02-26 21:07:08 -0500

    Update dependencies.

--- qubs_climate_data_api/settings.py
+++ qubs_climate_data_api/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
