48b1aa3acedaf1782f858a335ad99c83a33a18ed
David Lougheed <david.lougheed@gmail.com>
2017-06-06 15:59:59 -0400

    Rename qubs_climate_data_api to qubs_data_api.

--- qubs_data_api/wsgi.py
+++ qubs_data_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qubs_climate_data_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qubs_data_api.settings")
 
 application = get_wsgi_application()
