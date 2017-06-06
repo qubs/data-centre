90bd4fa168ffc210c32042fa75f538432681290a
David Lougheed <david.lougheed@gmail.com>
2017-06-06 17:04:08 -0400

    Add a specimen serializer and views. Mount the views on herbarium URLs.

--- qubs_data_api/urls.py
+++ qubs_data_api/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^climate/', include('climate.urls')),
+    url(r'^herbarium/', include('herbarium.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^admin/', admin.site.urls),
 ]
