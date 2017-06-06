f0626cc6ccdb80c83742026c8d066d879771eeb9
David Lougheed <david.lougheed@gmail.com>
2017-06-06 18:03:49 -0400

    Add Core app with users view and an API root directing users to sub-APIs.

---
+++ core/urls.py
@@ -0,0 +1,30 @@
+# Copyright 2016 the Queen's University Biological Station
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from django.conf.urls import url
+from rest_framework.documentation import include_docs_urls
+from core import views
+
+
+API_TITLE = 'QUBS Data API'
+API_DESCRIPTION = 'A web API for accessing data provided by the Queen\'s University Biological Station'
+
+
+urlpatterns = [
+    url(r'^$', views.api_root, name='qubs-api-root'),
+
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+]
