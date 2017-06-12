d1be7f345529594ba25ed5d0f22e544735a64404
David Lougheed <david.lougheed@gmail.com>
2017-06-12 12:42:25 -0400

    Add a custom admin site header.

--- qubs_data_centre/urls.py
+++ qubs_data_centre/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
+admin.site.site_header = 'QUBS Data Centre Admin'
+
 urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
