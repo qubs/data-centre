144b4ee57d9721668dada16afc3263799ab43184
David Lougheed <david.lougheed@gmail.com>
2017-06-21 11:47:23 -0400

    Set up static file collection / serving.

--- qubs_data_centre/urls.py
+++ qubs_data_centre/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -8,3 +10,7 @@ urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+# If in debug mode, serve static files from Django.
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
