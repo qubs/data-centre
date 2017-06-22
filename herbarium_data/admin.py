c3de2d799221270642e880f9499f1a65fe4da980
David Lougheed <david.lougheed@gmail.com>
2017-06-22 15:23:35 -0400

    Add list display, filtering, and default ordering to herbarium specimen admin display view.

--- herbarium_data/admin.py
+++ herbarium_data/admin.py
@@ -4,4 +4,7 @@ from herbarium_data.models import *
 
 @admin.register(Specimen)
 class SpecimenAdmin(admin.ModelAdmin):
+    list_display = ["accession", "dataset", "latin_name", "common_name", "date_collected_str"]
+    list_filter = ["dataset", "map_included"]
+    ordering = ["genus", "species"]
     pass
