9de142fa1a9186734e83aff4d2a6c88e0740c49f
David Lougheed <david.lougheed@gmail.com>
2017-07-27 15:39:27 -0400

    Switch directory names for collected static files and add a directory for global static files (templates, styles, etc.)

--- qubs_data_centre/settings.py
+++ qubs_data_centre/settings.py
@@ -141,7 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '{}/static/'.format(BASE_DIR)
+STATIC_ROOT = '{}/static_collected/'.format(BASE_DIR)
+STATICFILES_DIRS = (
+    '{}/static/'.format(BASE_DIR),
+)
 
 # REST Framework
 
