55f24dfe46b9f37f60d357dd2a465f4d070f40e0
David Lougheed <david.lougheed@gmail.com>
2017-08-02 19:54:50 -0400

    Move common filter lookups for herbarium specimens into tuples. Add querying by ID, dataset, map_included, collectors, notes, location, and habitat for herbarium specimens.

--- herbarium_data/filters.py
+++ herbarium_data/filters.py
@@ -1,18 +1,39 @@
 import django_filters
 
+from herbarium_data.models import *
+
+
+CHAR_FIELD_LOOKUPS = ('exact', 'iexact', 'icontains', 'istartswith', 'iendswith', 'in')
+TEXT_FIELD_LOOKUPS = ('exact', 'iexact', 'icontains', 'istartswith', 'iendswith')
+INTEGER_FIELD_LOOKUPS = ('exact', 'lt', 'lte', 'gt', 'gte', 'in')
+BOOLEAN_FIELD_LOOKUPS = ('exact',)
+
 
 class SpecimenFilter(django_filters.FilterSet):
     class Meta:
         model = Specimen
         fields = {
-            'genus': ('exact', 'iexact', 'icontains'),
-            'species': ('exact', 'iexact', 'icontains'),
-            'common_name': ('exact', 'iexact', 'icontains'),
+            'id': INTEGER_FIELD_LOOKUPS,
 
-            'dth': ('exact', 'iexact', 'icontains'),
-            'accession': ('exact', 'iexact', 'icontains'),
+            'dataset': CHAR_FIELD_LOOKUPS,
 
-            'year_collected': ('exact', 'lt', 'lte', 'gt', 'gte'),
-            'month_collected': ('exact', 'lt', 'lte', 'gt', 'gte'),
-            'day_collected': ('exact', 'lt', 'lte', 'gt', 'gte')
+            'genus': CHAR_FIELD_LOOKUPS,
+            'species': CHAR_FIELD_LOOKUPS,
+            'common_name': CHAR_FIELD_LOOKUPS,
+
+            'dth': CHAR_FIELD_LOOKUPS,
+            'accession': CHAR_FIELD_LOOKUPS,
+
+            'year_collected': INTEGER_FIELD_LOOKUPS,
+            'month_collected': INTEGER_FIELD_LOOKUPS,
+            'day_collected': INTEGER_FIELD_LOOKUPS,
+
+            'map_included': BOOLEAN_FIELD_LOOKUPS,
+
+            'collectors': TEXT_FIELD_LOOKUPS,
+
+            'location': CHAR_FIELD_LOOKUPS,
+            'habitat': CHAR_FIELD_LOOKUPS,
+
+            'notes': TEXT_FIELD_LOOKUPS
         }
