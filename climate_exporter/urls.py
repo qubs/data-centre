01f3128bc41c863cd468e5f89f2ad19d6be47bfc
David Lougheed <david.lougheed@gmail.com>
2018-01-05 17:24:01 -0500

    Name the climate exporter and climate manager index URLs for reference from other apps.

--- climate_exporter/urls.py
+++ climate_exporter/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from climate_exporter import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='climate-exporter-index'),
 ]
--- climate_manager/urls.py
+++ climate_manager/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from climate_manager import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='climate-manager-index'),
 
     url(r'^stations/(?P<pk>[0-9]+)/$', views.station_overview, name='station'),
     url(r'^stations/(?P<pk>[0-9]+)/(?P<data_type>[a-zA-Z0-9_]+)/$', views.station__data_type,
